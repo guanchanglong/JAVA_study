8232fe29a2f9df3f9e70f2a1a9a8628a98333efc
guanchanglong <506921079@qq.com>
2020-07-15 23:26:54 +0800

    7_15

---
+++ mybatis学习/Mybatis01/src/test/java/com/test/MybatisTest.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import com.dao.UserDao;
+import com.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class MybatisTest {
+    public static void main(String[]args)throws Exception{
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(inputStream);
+        SqlSession session = factory.openSession();
+        UserDao userDao = session.getMapper(UserDao.class);
+        List<User>users =userDao.findAll();
+        for (User user:users) {
+            System.out.println(users);
+        }
+        session.close();
+        inputStream.close();
+    }
+}
