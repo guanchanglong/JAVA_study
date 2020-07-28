dbe8ac2075d55a1400f809420235daf8f2d3ad0b
guanchanglong <506921079@qq.com>
2020-07-28 22:07:37 +0800

    LeetCode 263 丑数

---
+++ src/Project/Study/LeetCode/NumberOfUgly/Test.java
@@ -0,0 +1,20 @@
+package Project.Study.LeetCode.NumberOfUgly;
+
+class Solution {
+    public boolean isUgly(int num) {
+        if (num<1)
+            return false;
+        while (num%5==0){
+            num/=5;
+        }
+        while (num%3==0){
+            num/=3;
+        }
+        while (num%2==0){
+            num/=2;
+        }
+        return num == 1;
+    }
+}
+public class Test {
+}
