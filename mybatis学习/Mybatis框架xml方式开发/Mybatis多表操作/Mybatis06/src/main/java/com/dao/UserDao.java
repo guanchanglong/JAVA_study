49d0fdcd924666ca1da9c52404847ceb2d566b0f
guanchanglong <506921079@qq.com>
2020-08-07 19:58:04 +0800

    Mybatis多表操作中的多对多操作

---
+++ mybatis学习/Mybatis框架xml方式开发/Mybatis多表操作/Mybatis06/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import com.domain.User;
+
+import java.util.List;
+
+public interface UserDao {
+    /**
+     * 查询所有用户，同时获取到用户下所有账户的信息
+     * @return
+     */
+    List<User> findAll();
+
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+}
